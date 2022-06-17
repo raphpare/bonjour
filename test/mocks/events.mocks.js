6b7bcc277d88706cedfe87a4bd7794715af89bed
Raphaël Paré <raph.pare@outlook.com>
2022-06-17 17:45:30 -0400

    maj

--- test/mocks/events.mocks.js
+++ test/mocks/events.mocks.js
@@ -1,136 +1,197 @@
+export const generateDateFromNow = (day, heures = new Date().getHours, minutes = 0) => {
+    const dateNow = new Date();
+    return new Date(dateNow.getFullYear(), dateNow.getMonth(), dateNow.getDate() + day, heures, minutes);
+}
+
 export const EVENTS_MOCK = [
     {
-        id: 'ABC',
-        title: 'A',
-        subtitle: 'sub',
-        classNames: {
-            event: 'TEST-CLASS'
-        },
-        ariaLabel: 'text',
-        disabled: true,
+        id: '1',
+        title: 'Event 1',
+        subtitle: 'Subtitle',
         dateRange: {
-            start: new Date('2020-02-01T10:00:00'),
-            end: new Date(new Date().setHours(new Date().getHours() + 10))
+            start: generateDateFromNow(0, 10, 15),
+            end: generateDateFromNow(0, 12, 15)
+        },
+    },
+    {
+        id: '2',
+        title: 'Event 2',
+        dateRange: {
+            start: generateDateFromNow(0, 9, 30),
+            end: generateDateFromNow(0, 10, 30)
         }
     },
     {
-        id: 'sd53ms4dg',
-        title: 'Event A',
-        classNames: {
-            title: 'title-small'
-        },
+        id: '3',
+        title: 'Event 3',
         dateRange: {
-            start: new Date(new Date().setHours(new Date().getHours() - 6)),
-            end: new Date(new Date().setHours(new Date().getHours() + 2))
-        },
+            start: generateDateFromNow(0, 10),
+            end: generateDateFromNow(0, 13)
+        }
+    },
+    {
+        id: '4',
+        title: 'Event 4',
+        subtitle: 'subtitle',
+        dateRange: {
+            start: generateDateFromNow(0, 16, 30),
+            end: generateDateFromNow(0, 17, 30)
+        }
+    },
+    {
+        id: '5',
+        title: 'Event 5',
+        subtitle: 'subtitle',
+        dateRange: {
+            start: generateDateFromNow(-1, 16),
+            end: generateDateFromNow(-1, 17)
+        }
+    },
+    {
+        id: '6',
+        title: 'Event 6',
+        subtitle: 'subtitle',
+        dateRange: {
+            start: generateDateFromNow(-2, 8),
+            end: generateDateFromNow(-2, 14)
+        }
+    },
+    {
+        id: '7',
+        title: 'Event 7',
+        dateRange: {
+            start: generateDateFromNow(-2, 11),
+            end: generateDateFromNow(-2, 12)
+        }
+    },
+    {
+        id: '8',
+        title: 'Event 8',
+        dateRange: {
+            start: generateDateFromNow(-2, 9),
+            end: generateDateFromNow(-2, 13)
+        }
+    },
+    {
+        id: '9',
+        title: 'Event 9',
+        dateRange: {
+            start: generateDateFromNow(-1, 8),
+            end: generateDateFromNow(-1, 11)
+        }
+    },
+    {
+        id: '10',
+        title: 'Event 10',
+        dateRange: {
+            start: generateDateFromNow(0, 23, 30),
+            end: generateDateFromNow(1, 1)
+        }
+    },
+    {
+        id: '11',
+        title: 'Event 11',
+        dateRange: {
+            start: generateDateFromNow(1, 20, 30),
+            end: generateDateFromNow(2, 8)
+        }
+    },
+    {
+        id: '12',
+        title: 'Event 12',
+        dateRange: {
+            start: generateDateFromNow(3, 1),
+            end: generateDateFromNow(20, 5)
+        }
+    },
+    {
+        id: '13',
+        title: 'Event 13',
+        dateRange: {
+            start: generateDateFromNow(4, 10),
+            end: generateDateFromNow(10, 10)
+        }
+    },
+    {
+        id: '14',
+        title: 'Event 14',
+        dateRange: {
+            start: generateDateFromNow(4, 8),
+            end: generateDateFromNow(4, 11)
+        }
+    },
+    {
+        id: '15',
+        title: 'Event 15',
+        dateRange: {
+            start: generateDateFromNow(4, 13),
+            end: generateDateFromNow(4, 16)
+        }
+    },
+    {
+        id: '16',
+        title: 'Event 16',
         allDay: true,
-    },
-    {
-        id: '28g33e6',
-        title: 'C-',
         dateRange: {
-            start: new Date(new Date().setHours(new Date().getHours() - 5)),
-            end: new Date(new Date().setHours(new Date().getHours() - 4))
+            start: generateDateFromNow(0, 0),
+            end: generateDateFromNow(0, 0)
         }
     },
     {
-        id: 'vda6',
-        title: 'C',
+        id: '17',
+        title: 'Event 17',
+        allDay: true,
         dateRange: {
-            start: new Date(new Date().setHours(new Date().getHours() - 6)),
-            end: new Date(new Date().setHours(new Date().getHours() - 4))
+            start: generateDateFromNow(0, 0),
+            end: generateDateFromNow(2, 0)
         }
     },
     {
-        id: '06-22ed4-ffdd',
-        title: 'B',
-        classNames: {
-            event: 'TEST-CLASS'
-        },
+        id: '18',
+        title: 'Event 18',
+        allDay: true,
         dateRange: {
-            start: new Date(new Date().setHours(new Date().getHours() - 2)),
-            end: new Date(new Date().setHours(new Date().getHours() + 1))
+            start: generateDateFromNow(4, 0),
+            end: generateDateFromNow(10, 0)
         }
     },
     {
-        id: '4f76dw5ga',
-        title: 'B-1',
+        id: '18',
+        title: 'Event 18',
+        allDay: true,
         dateRange: {
-            start: new Date(new Date().setHours(new Date().getHours() - 2)),
-            end: new Date(new Date().setHours(new Date().getHours() + 1))
+            start: generateDateFromNow(-4, 0),
+            end: generateDateFromNow(-8, 0)
         }
     },
     {
-        id: 'f76dw0a',
-        title: 'Event B-2',
-        classNames: {
-            event: 'TEST-CLASS'
-        },
+        id: '19',
+        title: 'Event 19',
+        allDay: true,
         dateRange: {
-            start: new Date(new Date().setHours(new Date().getHours() - 1)),
-            end: new Date(new Date().setHours(new Date().getHours() + 1))
+            start: generateDateFromNow(-4, 0),
+            end: generateDateFromNow(-4, 0)
         }
     },
     {
-        id: 'aaa',
-        title: 'B-3',
+        id: '20',
+        title: 'Event 20',
+        allDay: true,
         dateRange: {
-            start: new Date(new Date().setHours(new Date().getHours() - 24)),
-            end: new Date(new Date().setHours(new Date().getHours() + 10074))
+            start: generateDateFromNow(-4, 0),
+            end: generateDateFromNow(-3, 0)
+        }
+    },
+     {
+        id: '21',
+        title: 'Event 21',
+        allDay: true,
+        dateRange: {
+            start: generateDateFromNow(-3, 0),
+            end: generateDateFromNow(-3, 0)
         }
     },
     
-    {
-        id: '3de6jtgsd',
-        title: 'Event D',
-        dateRange: {
-            start: new Date('2022-04-04 12:30:30'),
-            end: new Date('2022-04-04 15:30:30')
-        }
-    },
-    {
-        id: '3de6jtgsd',
-        title: 'Event D',
-        dateRange: {
-            start: new Date('2022-04-04 12:30:30'),
-            end: new Date('2022-04-04 15:30:30')
-        }
-    },
-    {
-        id: '2ydf5r46',
-        title: 'Event E',
-        dateRange: {
-            start: new Date('2021-06-06 12:30:30'),
-            end: new Date('2021-06-06 15:30:30')
-        }
-    },
-    {
-        id: '427ssits68hg',
-        title: 'Event F',
-        dateRange: {
-            start: new Date(),
-            end: new Date('2022-06-25 12:30:30')
-        },
-        allDay: true,
-    },
-    {
-        id: '5sfg6gf',
-        title: 'Event G',
-        dateRange: {
-            start: new Date(),
-            end: new Date()
-        },
-        allDay: true,
-    },
-    {
-        id: '5frge6rdsh',
-        title: 'Event H',
-        dateRange: {
-            start: new Date('2021-06-10 12:30:30'),
-            end: new Date()
-        },
-        allDay: true,
-    },
 ];
 
 export const EVENTS_MOCK_2 = [
